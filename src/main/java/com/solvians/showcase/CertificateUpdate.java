7b402fd44ff50f30e98b1c0b80fceba494432ae8
noor <masrur.noor@tigerit.com>
2026-05-20 21:01:52 +0600

    Implement CertificateUpdate construction

--- src/main/java/com/solvians/showcase/CertificateUpdate.java
+++ src/main/java/com/solvians/showcase/CertificateUpdate.java
@@ -1,6 +1,15 @@
 package com.solvians.showcase;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class CertificateUpdate {
+    private static final int MIN_PRICE = 100;
+    private static final int MAX_PRICE = 200;
+    private static final int MIN_BID_SIZE = 1000;
+    private static final int MAX_BID_SIZE = 5000;
+    private static final int MIN_ASK_SIZE = 1000;
+    private static final int MAX_ASK_SIZE = 10000;
+
     private final long timestamp;
     private final String isin;
     private final double bidPrice;
@@ -15,10 +24,10 @@ public class CertificateUpdate {
     public CertificateUpdate(IsinGenerator isinGenerator) {
         this.timestamp = System.currentTimeMillis();
         this.isin = isinGenerator.generate();
-        this.bidPrice = 101.23;
-        this.bidSize = 1000;
-        this.askPrice = 103.45;
-        this.askSize = 1000;
+        this.bidPrice = randomPrice();
+        this.bidSize = randomBidSize();
+        this.askPrice = randomPrice();
+        this.askSize = randomAskSize();
     }
 
     public long getTimestamp() {
@@ -43,5 +52,17 @@ public class CertificateUpdate {
 
     public int getAskSize() {
         return askSize;
+    }
+
+    private double randomPrice() {
+        return ThreadLocalRandom.current().nextInt(MIN_PRICE*100, MAX_PRICE*100 + 1) / 100.0;
+    }
+
+    private int randomAskSize() {
+        return ThreadLocalRandom.current().nextInt(MIN_ASK_SIZE, MAX_ASK_SIZE + 1);
+    }
+
+    private int randomBidSize() {
+        return ThreadLocalRandom.current().nextInt(MIN_BID_SIZE, MAX_BID_SIZE+1);
     }
 }
